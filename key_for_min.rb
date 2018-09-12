0de140a65ee1b08ccc3196d95e3e7e8c21717cf5
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-12 13:35:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   k = nil
   v = 0
-  name_hash.collect do |key, value|
+  name_hash.map do |key, value|
     if v == 0 || value < v
       v = value
       k = key
